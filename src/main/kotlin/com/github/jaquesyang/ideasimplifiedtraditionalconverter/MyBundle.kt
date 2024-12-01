a8d9d120be9f9033aa7ce88b2c3bbc4aa7dd27d5
GitHub Action <action@github.com>
2024-12-01 05:37:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/jaquesyang/ideasimplifiedtraditionalconverter/MyBundle.kt
+++ src/main/kotlin/com/github/jaquesyang/ideasimplifiedtraditionalconverter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jaquesyang.ideasimplifiedtraditionalconverter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
